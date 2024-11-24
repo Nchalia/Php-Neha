ed3918fabfb733ab036ef5375e4b1569f89a0754
Nchalia <108783361+Nchalia@users.noreply.github.com>
2024-11-24 22:31:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+// Hardcoded credentials example
+$db_user = "admin";
+$db_password = "123456"; // Security vulnerability
 echo "Hello, World! Welcome to your first PHP app with GitHub Actions!";
 ?>
